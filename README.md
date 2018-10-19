2f5ddec25a4aeab804b273630c13bae14db27ef1
zc.ding <zhichaoding@hongkun.com.cn>
2018-10-19 10:41:40 +0800

    配置https

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # app-server
 app 版本管控台
+
+要求
+    jdk1.8+
+
+生成证书
+    keytool -genkey -alias tomcat -storetype PKCS12 -keyalg RSA -keysize 2048 -keystore keystore.p12 -validity 3650
+        
+启动
+    java -jar name.jar --server.port=端口 --spring.profiles.active=test
--- src/main/java/com/dobe/appserver/AppServerApplication.java
+++ src/main/java/com/dobe/appserver/AppServerApplication.java
@@ -1,11 +1,53 @@
 package com.dobe.appserver;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
+/**
+*  启动项
+*  @date                    ：2018/8/10
+*  @author                  ：zc.ding@foxmail.com
+*/
 @SpringBootApplication
 public class AppServerApplication {
-	public static void main(String[] args) {
-	    SpringApplication.run(AppServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppServerApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        //tomcat初始化方式决定是否允许http、https同时访问项目，如果调用午餐的构造方法，那么8080/8443可单独进行访问
+        //如果调用如下有参的构造方法，那么即使访问8080也会被重定向8443端口
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        
+        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
+        return tomcat;
+    }
+
+    private Connector createHTTPConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        //同时启用http（8080）、https（8443）两个端口
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(8080);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
 }
